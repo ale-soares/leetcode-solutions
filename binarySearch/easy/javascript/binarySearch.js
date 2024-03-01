0f4a21d002fe3a0a605658318db636823356272f
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-01 17:02:14 -0300

    add binary search

---
+++ binarySearch/easy/javascript/binarySearch.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * Time O(log(N)) | Space O(1)
+ * @return {number}
+ */
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left + 1) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
