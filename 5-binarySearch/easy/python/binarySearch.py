7a6c3a145f3da733510ca44d3ac76765029b7117
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-04 18:01:21 -0300

    change binary search python code

--- 5-binarySearch/easy/python/binarySearch.py
+++ 5-binarySearch/easy/python/binarySearch.py
@@ -5,11 +5,14 @@ class Solution:
 		l, r = 0, len(nums) - 1
 
 		while l <= r:
-			m = l + ((r - l) // 2)  # (l + r) // 2 can lead to overflow
-			if nums[m] > target:
-				r = m - 1
-			elif nums[m] < target:
-				l = m + 1
+			mid = (l + (r - l)//2)
+
+			if nums[mid] == target:
+				return mid
+			elif nums[mid] > target:
+				r = mid - 1
 			else:
-				return m
+				l = mid + 1
+
 		return -1
+        
