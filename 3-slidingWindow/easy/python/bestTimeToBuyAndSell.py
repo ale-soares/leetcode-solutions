89456b99f5532d90591306b5236db67b2e99a713
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-07 16:31:00 -0300

    add two pointer solution to best time to buy and sell python problem

--- 3-slidingWindow/easy/python/bestTimeToBuyAndSell.py
+++ 3-slidingWindow/easy/python/bestTimeToBuyAndSell.py
@@ -10,3 +10,20 @@ class Solution:
         lowest = price
       res = max(res, price - lowest)
     return res
+  
+# Two pointer solution
+class Solution:
+  def maxProfit(self, prices: List[int]) -> int:
+    buy, sell, max_profit = 0, 1, 0
+
+    while sell < len(prices):
+      #if profitable
+      if (prices[buy] < prices[sell]):
+        profit = prices[sell] - prices[buy]
+        max_profit = max(max_profit, profit)
+      #if not profitable
+      else:
+        buy = sell
+      sell += 1
+
+    return max_profit
