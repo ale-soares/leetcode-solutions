f8b0be4277e62b2afc2b4551ba91a4e07c51dbeb
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-13 17:11:33 -0300

    add another solution for balanced binary tree exercise

--- 7-trees/easy/balancedBinaryTree.py
+++ 7-trees/easy/balancedBinaryTree.py
@@ -39,3 +39,22 @@ class Solution:
       return [is_balanced, 1 + height_from_node]
 
     return dfs(root)[0]
+
+# 2: Using dictionary to access values by key
+class Solution:
+  def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    # dfs return dict with 2 values, first is bool representation of balance and second is tree height
+    def dfs(node):
+      if not node: 
+          return {"balanced": True, "height": 0}
+
+      left, right = dfs(node.left), dfs(node.right)
+
+      sides_height_diff = abs(left["height"] - right["height"])
+
+      is_balanced = (left["balanced"] and right["balanced"] and sides_height_diff <= 1)
+      height_from_node = max(left["height"], right["height"])
+
+      return {"balanced": is_balanced, "height": 1 + height_from_node}
+
+    return dfs(root)["balanced"]
