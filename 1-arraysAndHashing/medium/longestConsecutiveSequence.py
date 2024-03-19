c7df0878d6e9780eece68cd58ae0bc4c17a64283
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-18 21:33:11 -0300

    add second solution to longest consecutive sequence

--- 1-arraysAndHashing/medium/longestConsecutiveSequence.py
+++ 1-arraysAndHashing/medium/longestConsecutiveSequence.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# Solution using start and end
 class Solution:
   def longestConsecutive(self, nums: List[int]) -> int:
     num_set = set(nums)
@@ -13,3 +14,18 @@ class Solution:
         length = max(length, end - start)
     
     return length
+  
+# Solution using length and longest
+  class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+      num_set = set(nums)
+      longest = 0
+
+      for n in nums:
+        if (n - 1) not in num_set:
+          length = 1
+          while (n + length) in num_set:
+            length += 1
+          longest = max(length, longest)
+      
+      return longest
