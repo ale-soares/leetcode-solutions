03837386d9238571bc396cd93004214c0c6795eb
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-21 17:58:23 -0300

    add another solution to two sum with sorted arr

--- 2-twoPointers/medium/twoSumSortedArray.py
+++ 2-twoPointers/medium/twoSumSortedArray.py
@@ -15,6 +15,18 @@ class Solution:
       currentSum = numbers[left] + numbers[right]
 
     return [left + 1, right + 1]
+  
+# Two pointer solution using left and right pointers (simplified)
+class Solution:
+  def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    l, r = 0, len(numbers) - 1
+
+    while l < r:
+      curSum = numbers[l] + numbers[r]
+
+      if curSum > target: r -= 1
+      elif curSum < target: l += 1
+      else: return [l + 1, r + 1]
 
 # Solution using hashmap to store value and index
 class Solution:
