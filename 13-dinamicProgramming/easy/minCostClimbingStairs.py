1856211171fcf75499dbd4906d73c81ed886e4e9
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-22 20:26:31 -0300

    add recursive solution to min cost climbing stairs

--- 13-dinamicProgramming/easy/minCostClimbingStairs.py
+++ 13-dinamicProgramming/easy/minCostClimbingStairs.py
@@ -19,3 +19,21 @@ class Solution:
       cost[i] += min(cost[i - 1], cost[i - 2])
 
     return min(cost[n - 1], cost[n - 2])
+  
+# Solution using recursion and memoization
+class Solution:
+  def minCostClimbingStairs(self, cost: List[int]) -> int:
+    cost.append(0)
+    steps = len(cost) - 1
+    memo = {}
+
+    def helper(step, memo):
+      if step <= 1: return cost[step]
+
+      if step not in memo: 
+        min_step = min(helper(step - 1, memo), helper(step - 2, memo))
+        memo[step] = cost[step] + min_step
+      
+      return memo[step]
+
+    return helper(steps, memo)
