fe1f2b4f77ae9ba778a6ce24acd46d13dfe6f7f3
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-25 15:43:03 -0300

    add dp solution to longest palindromic substring

--- 13-dinamicProgramming/medium/longestPalindromicSubstring.py
+++ 13-dinamicProgramming/medium/longestPalindromicSubstring.py
@@ -15,3 +15,32 @@ class Solution:
         r += 1
 
     return substr
+  
+# Solution using dynamic programming
+class Solution:
+  def longestPalindrome(self, s: str) -> str:
+    n = len(s)
+    start = 0
+    max_len = 1
+    dp = [[False] * n for _ in range(n)]
+
+    for i in range(n):
+      dp[i][i] = True
+
+    # Check substrings of length 2
+    for i in range(n - 1):
+      if s[i] == s[i + 1]:
+        dp[i][i + 1] = True
+        start = i
+        max_len = 2
+
+    # Check substrings of length 3 or more
+    for length in range(3, n + 1):
+      for i in range(n - length + 1):
+        j = i + length - 1
+        if dp[i + 1][j - 1] and s[i] == s[j]:
+            dp[i][j] = True
+            start = i
+            max_len = length
+
+    return s[start:start + max_len]
