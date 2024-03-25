6d4b5a6f0a192dae6a43cdf83866a1b79f5b33a2
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-25 12:19:38 -0300

    add solution with comments and new variables to car fleet exercise

--- 4-stack/medium/carFleet.py
+++ 4-stack/medium/carFleet.py
@@ -1,5 +1,22 @@
 from typing import List
 
+# Solution using reversed and more variables to improve readability
+class Solution:
+  def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+    pair = [(p, s) for p, s in zip(position, speed)]
+    car_fleets = []
+    
+    for p, s in reversed(sorted(pair)):
+      car_arrival_time = (target - p) / s
+      car_fleets.append(car_arrival_time)
+
+      # if a car in the back takes the same amount of time or
+      # less to arrive as the one in the front, they are a car fleet
+      if len(car_fleets) >= 2 and car_fleets[-1] <= car_fleets[-2]:
+        car_fleets.pop()
+
+    return len(car_fleets)
+
 class Solution:
   def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
     pair = [(p, s) for p, s in zip(position, speed)]
