6c2389962541d9d310c82492db2022596705f7fa
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-29 19:24:47 -0300

    add another solution to reorder list exercise

--- 6-linkedList/medium/reorderList.py
+++ 6-linkedList/medium/reorderList.py
@@ -11,6 +11,36 @@ class ListNode:
     self.val = val
     self.next = next
 
+# Solution using single function
+class Solution:
+  def reorderList(self, head: ListNode) -> None:
+    # find middle
+    slow, fast = head, head.next
+
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+
+    # reverse second half
+    second = slow.next
+    prev = slow.next = None
+
+    while second:
+      tmp = second.next
+      second.next = prev
+      prev = second
+      second = tmp
+
+    # merge two halfs in alternating pattern
+    first, second = head, prev
+
+    while second:
+      tmp1, tmp2 = first.next, second.next
+      first.next = second
+      second.next = tmp1
+      first, second = tmp1, tmp2
+
+# Solution using multiple functions
 class Solution:
   def reverse(self, head):
     if not head:
