c981a747204c4c05c17812976a4ae3b5731c8a7a
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-31 18:01:51 -0300

    add comment to diameter of binary tree exercise

--- 7-trees/easy/diameterOfBinaryTree.py
+++ 7-trees/easy/diameterOfBinaryTree.py
@@ -7,15 +7,18 @@ class TreeNode:
     self.left = left
     self.right = right
 
+# Recursive DFS solution | Time complexity: O(n), Space complexity: O(n)
 class Solution:
   def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
     diameter = 0
 
     def dfs(root):
+      # at each node, calculate the length of the longest path passing through that node
+      # which is the sum of the heights of its left and right subtrees
+      # keep track of the maximum diameter encountered so far
       nonlocal diameter
 
-      if not root:
-        return 0
+      if not root: return 0
 
       left_height, right_height = dfs(root.left), dfs(root.right)
       diameter = max(diameter, left_height + right_height)
