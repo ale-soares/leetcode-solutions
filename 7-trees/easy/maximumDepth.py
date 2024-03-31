de58cefbdc98548f42acd4f0930056ef10d30a6c
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-03-31 17:01:40 -0300

    add comments and extra solution to maximum depth exercise

--- 7-trees/easy/maximumDepth.py
+++ 7-trees/easy/maximumDepth.py
@@ -7,6 +7,9 @@ class TreeNode:
     self.left = left
     self.right = right
 
+# Recursive solution using depth variable
+# Time Complexity - O(n) where n is number of nodes
+# Space Complexity: O(h) where h is height of the tree (recursive stack)
 class Solution:
   def maxDepth(self, root: Optional[TreeNode], depth = 0) -> int:
     if not root:
@@ -16,3 +19,15 @@ class Solution:
     rightDepth = self.maxDepth(root.right, depth + 1)
 
     return max(leftDepth, rightDepth)
+  
+# Recursive solution without depth variable
+# Time Complexity - O(n) where n is number of nodes
+# Space Complexity: O(h) where h is height of the tree (recursive stack)
+class Solution:
+  def maxDepth(self, root: Optional[TreeNode]) -> int:
+    if not root: return 0
+
+    left_depth = self.maxDepth(root.left)
+    right_depth = self.maxDepth(root.right)
+
+    return max(left_depth, right_depth) + 1
