527455a4e742cecfea60b9964c81f307c27ac2b3
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-04-01 18:26:40 -0300

    add comments and extra solution for two sum exercise

--- 1-arraysAndHashing/easy/python/twoSum.py
+++ 1-arraysAndHashing/easy/python/twoSum.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# Time complexity: O(n), Space complexity: O(n)
 class Solution:
   def twoSum(self, nums: List[int], target: int) -> List[int]:
     indexes = {}
@@ -9,4 +10,19 @@ class Solution:
 
       if (complement in indexes):
         return [indexes[complement], i]
+
       indexes[n] = i
+
+# Time complexity: O(n), Space complexity: O(n)
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    numMap = {}
+    n = len(nums)
+
+    for i in range(n):
+      numMap[nums[i]] = i
+
+    for i in range(n):
+      complement = target - nums[i]
+      if complement in numMap and numMap[complement] != i:
+        return [i, numMap[complement]]
