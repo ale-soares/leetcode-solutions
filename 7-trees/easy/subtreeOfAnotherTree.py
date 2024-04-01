f4671fff870343a1f31c3c8b5f0b37dbb7f21a12
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-04-01 16:21:23 -0300

    add solution and comments to subtree of another tree exercise

--- 7-trees/easy/subtreeOfAnotherTree.py
+++ 7-trees/easy/subtreeOfAnotherTree.py
@@ -7,6 +7,9 @@ class TreeNode:
     self.left = left
     self.right = right
 
+# Shorter solution with two separate functions
+# Time complexity: O(r * s) -> r is the number of nodes in the root tree and s is the number of nodes in the subRoot tree
+# Space complexity: O(h) -> where h is the height of the root tree due to the recursive calls
 class Solution:
   def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
     if not root: return False
@@ -18,3 +21,23 @@ class Solution:
     if r and s:
       return r.val == s.val and self.isSameTree(r.left, s.left) and self.isSameTree(r.right, s.right) 
     return r is s
+  
+# Longer solution with two separate functions
+# Time complexity: O(r * s) -> r is the number of nodes in the root tree and s is the number of nodes in the subRoot tree
+# Space complexity: O(h) -> where h is the height of the root tree due to the recursive calls
+class Solution:
+  def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    if not subRoot: return True
+    if not root: return False
+
+    if self.isSameTree(root, subRoot): return True
+    return self.isSubtree(root.left , subRoot) or self.isSubtree(root.right , subRoot)
+
+  def isSameTree(self, r, s):
+    if not r and not s: return True
+    if not r or not s or r.val != s.val: return False
+
+    same_right = self.isSameTree(r.right, s.right)
+    same_left = self.isSameTree(r.left, s.left)
+
+    return same_right and same_left
