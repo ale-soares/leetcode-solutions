da96dd6bd9a667a2afcc54ebba928b69fae67a8b
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-05-21 11:41:43 -0300

    add copy list with random pointer

---
+++ 6-linkedList/medium/copyListWithRandomPointer.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+# Definition for a Node.
+class Node:
+  def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+    self.val = int(x)
+    self.next = next
+    self.random = random
+
+# Solution using hashmap to map old with new | Time complexity: O(n), Space complexity: O(n)
+class Solution:
+  def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    oldToCopy = { None : None }
+
+    cur = head
+    while cur:
+      copy = Node(cur.val)
+      oldToCopy[cur] = copy
+      cur = cur.next
+
+    cur = head
+    while cur:
+      copy = oldToCopy[cur]
+      copy.next = oldToCopy[cur.next]
+      copy.random = oldToCopy[cur.random]
+      cur = cur.next
+
+    return oldToCopy[head]
